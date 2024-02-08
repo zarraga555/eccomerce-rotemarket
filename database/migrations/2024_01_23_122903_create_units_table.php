8ec4633f1341307358b0776ba12ca4e8f9139b5b
Alberto Zarraga <zarraga555@gmail.com>
2024-02-08 13:02:05 -0400

    Se agregagaron nuevas migraciones

--- database/migrations/2024_01_23_122903_create_units_table.php
+++ database/migrations/2024_01_23_122903_create_units_table.php
@@ -11,16 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('units', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->string('type_user')->default('client');
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('description')->nullable();
+            $table->foreignId('creat_by')->nullable()->constrained('users');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -31,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('units');
     }
 };
