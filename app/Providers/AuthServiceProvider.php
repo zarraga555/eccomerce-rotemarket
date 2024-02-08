01d5b32a4df3d86e3c9cf039285ed0fdeb287429
Alberto Zarraga <zarraga555@gmail.com>
2024-02-08 13:04:52 -0400

    Se agregaron las politicas para la seguridad

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,23 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\Brands;
+use App\Models\Categories;
+use App\Models\CategoryDepartment;
+use App\Models\Products;
+use App\Models\SubCategories;
+use App\Models\Units;
+use App\Models\User;
+use App\Policies\BrandsPolicy;
+use App\Policies\CategoriesPolicy;
+use App\Policies\CategoryDepartmentPolicy;
+use App\Policies\ProductsPolicy;
+use App\Policies\RolePolicy;
+use App\Policies\SubCategoriesPolicy;
+use App\Policies\UnitsPolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Spatie\Permission\Models\Role;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +29,14 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        User::class                 => UserPolicy::class,
+        Role::class                 => RolePolicy::class,
+        Categories::class           => CategoriesPolicy::class,
+        CategoryDepartment::class   => CategoryDepartmentPolicy::class,
+        SubCategories::class        => SubCategoriesPolicy::class,
+        Brands::class               => BrandsPolicy::class,
+        Products::class             => ProductsPolicy::class,
+        Units::class                => UnitsPolicy::class
     ];
 
     /**
