152a0952d855fa271f273157260df13601f5369e
Alberto Zarraga <zarraga555@gmail.com>
2024-02-08 12:58:40 -0400

    Se agreego el resource para categorias

---
+++ app/Filament/Resources/CategoriesResource/Pages/EditCategories.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\CategoriesResource\Pages;
+
+use App\Filament\Resources\CategoriesResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCategories extends EditRecord
+{
+    protected static string $resource = CategoriesResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
