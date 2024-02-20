20bd07f149741c3c8ee6b314d296e841afdcca1d
Alberto Zarraga <zarraga55@gmail.com>
2024-02-20 11:38:06 -0400

    Se agrego el resource de filamnet para los clientes

---
+++ app/Filament/Resources/CustomersResource/Pages/EditCustomers.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\CustomersResource\Pages;
+
+use App\Filament\Resources\CustomersResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCustomers extends EditRecord
+{
+    protected static string $resource = CustomersResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
---
+++ app/Filament/Resources/CustomersResource/Pages/ListCustomers.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\CustomersResource\Pages;
+
+use App\Filament\Resources\CustomersResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListCustomers extends ListRecords
+{
+    protected static string $resource = CustomersResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
