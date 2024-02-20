1021429ef7cb6d63f07282b16343c13885f3cfc6
Alberto Zarraga <zarraga555@gmail.com>
2024-02-20 11:53:18 -0400

    Se agregaron las politicas para el uso de clientes, empleados,. y proveedores

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,16 +7,21 @@ use App\Models\Brands;
 use App\Models\Coupons;
 use App\Models\Categories;
 use App\Models\CategoryDepartment;
+use App\Models\Customers;
+use App\Models\Employees;
 use App\Models\Products;
 use App\Models\SubCategories;
+use App\Models\Suppliers;
 use App\Models\Units;
 use App\Models\User;
 use App\Policies\BrandsPolicy;
 use App\Policies\CategoriesPolicy;
 use App\Policies\CategoryDepartmentPolicy;
+use App\Policies\CustomerPolicy;
 use App\Policies\ProductsPolicy;
 use App\Policies\RolePolicy;
 use App\Policies\SubCategoriesPolicy;
+use App\Policies\SuppliersPolicy;
 use App\Policies\UnitsPolicy;
 use App\Policies\UserPolicy;
 use App\Policies\CouponsPolicy;
@@ -40,6 +45,9 @@ class AuthServiceProvider extends ServiceProvider
         Products::class             => ProductsPolicy::class,
         Units::class                => UnitsPolicy::class,
         Coupons::class              => CouponsPolicy::class,
+        Customers::class            => CustomerPolicy::class,
+        Suppliers::class            => SuppliersPolicy::class,
+        Employees::class            => SuppliersPolicy::class
     ];
 
     /**
