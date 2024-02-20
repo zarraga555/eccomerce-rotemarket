c62d2f54f0fa50b383ef28c8fb9172647bc89756
Alberto Zarraga <zarraga555@gmail.com>
2024-02-20 11:38:23 -0400

    Se agrego el resource de filament para el uso de empleados

---
+++ app/Filament/Resources/EmployeesResource/Pages/ListEmployees.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\EmployeesResource\Pages;
+
+use App\Filament\Resources\EmployeesResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListEmployees extends ListRecords
+{
+    protected static string $resource = EmployeesResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
