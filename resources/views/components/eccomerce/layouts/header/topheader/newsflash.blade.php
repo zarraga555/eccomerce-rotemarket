cf2e6ff51c5288ca622cd4c13ff534f45a76e332
Alberto Zarraga <zarraga555@gmail.com>
2024-03-02 00:23:28 -0400

    Se convirtio en un componente la parte de las noticias flash para agrgar opciones en un futuro

---
+++ resources/views/components/eccomerce/layouts/header/topheader/newsflash.blade.php
@@ -0,0 +1,3 @@
+<li>{{__("100% Secure delivery without contacting the courier")}}</li>
+<li>{{__("Supper Value Deals - Save more with coupons")}}</li>
+<li>{{__("Trendy 25 silver jewelry, save up 35% off today")}}</li>
