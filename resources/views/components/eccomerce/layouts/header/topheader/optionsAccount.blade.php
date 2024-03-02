431925a2174c250a510033a525f089a855813392
Alberto Zarraga <zarraga555@gmail.com>
2024-03-02 00:23:46 -0400

    Se convirtio en un componente la parte de las opciones de cuenta del usuario para agrgar opciones en un futuro

---
+++ resources/views/components/eccomerce/layouts/header/topheader/optionsAccount.blade.php
@@ -0,0 +1,4 @@
+<li><a href="page-about.htlm">{{__('About Us')}}</a></li>
+<li><a href="page-account.html">{{__('My Account')}}</a></li>
+<li><a href="shop-wishlist.html">{{__('Wishlist')}}</a></li>
+{{-- <li><a href="shop-order.html">{{__("Order Tracking")}}</a></li> --}}
