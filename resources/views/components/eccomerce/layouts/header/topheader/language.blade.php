b4dc395b6eae71967791ffb73a96be2d29517178
Alberto Zarraga <zarraga555@gmail.com>
2024-03-02 00:23:13 -0400

    Se convirtio en un componente la parte del los idiomas para agrgar opciones en un futuro

---
+++ resources/views/components/eccomerce/layouts/header/topheader/language.blade.php
@@ -0,0 +1,29 @@
+                        {{-- <li>
+                            <a class="language-dropdown-active" href="#">English <i
+                                    class="fi-rs-angle-small-down"></i></a>
+                            <ul class="language-dropdown">
+                                <li>
+                                    <a href="#"><img src="assets/imgs/theme/flag-fr.png" alt=""/>Français</a>
+                                </li>
+                                <li>
+                                    <a href="#"><img src="assets/imgs/theme/flag-dt.png" alt=""/>Deutsch</a>
+                                </li>
+                                <li>
+                                    <a href="#"><img src="assets/imgs/theme/flag-ru.png" alt=""/>Pусский</a>
+                                </li>
+                            </ul>
+                        </li> --}}
+                        {{-- <li>
+                            <a class="language-dropdown-active" href="#">USD <i class="fi-rs-angle-small-down"></i></a>
+                            <ul class="language-dropdown">
+                                <li>
+                                    <a href="#"><img src="assets/imgs/theme/flag-fr.png" alt=""/>INR</a>
+                                </li>
+                                <li>
+                                    <a href="#"><img src="assets/imgs/theme/flag-dt.png" alt=""/>MBP</a>
+                                </li>
+                                <li>
+                                    <a href="#"><img src="assets/imgs/theme/flag-ru.png" alt=""/>EU</a>
+                                </li>
+                            </ul>
+                        </li> --}}
