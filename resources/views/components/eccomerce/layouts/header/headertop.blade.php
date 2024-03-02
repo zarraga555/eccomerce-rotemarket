91955126ceb4725adf2b67f0c8bf940fdc4c0612
Alberto Zarraga <zarraga555@gmail.com>
2024-03-02 00:24:13 -0400

    heder footer

---
+++ resources/views/components/eccomerce/layouts/header/headertop.blade.php
@@ -0,0 +1,30 @@
+<div class="header-top header-top-ptb-1 d-none d-lg-block">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-xl-3 col-lg-4">
+                <div class="header-info">
+                    <ul>
+                        <x-eccomerce.layouts.header.topheader.optionsAccount/>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-4">
+                <div class="text-center">
+                    <div id="news-flash" class="d-inline-block">
+                        <ul>
+                            <x-eccomerce.layouts.header.topheader.newsflash/>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4">
+                <div class="header-info header-info-right">
+                    <ul>
+                        <x-eccomerce.layouts.header.topheader.whatsapptelf/>
+                        <x-eccomerce.layouts.header.topheader.language/>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
