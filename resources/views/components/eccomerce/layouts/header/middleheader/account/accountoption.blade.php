88cb6761d38262ac880e366853419e4a5dba8449
Alberto Zarraga <zarraga555@gmail.com>
2024-03-01 23:05:16 -0400

    Se convirtio en un componente la parte de la cuenta del usuario para agrgar opciones en un futuro

---
+++ resources/views/components/eccomerce/layouts/header/middleheader/account/accountoption.blade.php
@@ -0,0 +1,22 @@
+<div class="header-action-icon-2">
+    <a href="page-account.html">
+        <img class="svgInject" alt="Nest" src="assets/imgs/theme/icons/icon-user.svg"/>
+    </a>
+    <a href="page-account.html"><span class="lable ml-0">{{__("Account")}}</span></a>
+    <div class="cart-dropdown-wrap cart-dropdown-hm2 account-dropdown">
+        @auth
+        <ul>
+            <x-eccomerce.layouts.header.middleheader.account.listAccount/>
+        </ul>
+        @endauth
+        
+        @guest
+            <ul>
+                <li>
+                    <a href="page-login.html"><i class="fi fi-rs-sign-out mr-10"></i>{{__("Login")}}</a>
+                </li>
+            </ul>
+        @endguest
+
+    </div>
+</div>
