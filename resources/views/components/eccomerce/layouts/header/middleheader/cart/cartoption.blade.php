586ee9378796db899c3b6647be94c52fe6ed6459
Alberto Zarraga <zarraga555@gmail.com>
2024-03-02 00:22:53 -0400

    Se convirtio en un componente la parte del carrito para agrgar opciones en un futuro

---
+++ resources/views/components/eccomerce/layouts/header/middleheader/cart/cartoption.blade.php
@@ -0,0 +1,12 @@
+<div class="header-action-icon-2">
+    <x-eccomerce.layouts.header.middleheader.cart.badge/>
+    <a href="shop-cart.html"><span class="lable">{{__("Cart")}}</span></a>
+    <div class="cart-dropdown-wrap cart-dropdown-hm2">
+        <ul>
+            <x-eccomerce.layouts.header.middleheader.cart.cartdetails/>
+        </ul>
+        <div class="shopping-cart-footer">
+            <x-eccomerce.layouts.header.middleheader.cart.shopppingcartfooter/>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/eccomerce/layouts/header/middleheader/cart/shopppingcartfooter.blade.php
@@ -0,0 +1,7 @@
+<div class="shopping-cart-total">
+    <h4>Total <span>$4000.00</span></h4>
+</div>
+<div class="shopping-cart-button">
+    <a href="shop-cart.html" class="outline">{{__("View cart")}}</a>
+    <a href="shop-checkout.html">{{__("Checkout")}}</a>
+</div>
