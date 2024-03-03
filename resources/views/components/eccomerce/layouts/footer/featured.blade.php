2f5a19a68ed430e0a8f6ee91b164abd5dda574e2
Alberto Zarraga <zarraga555@gmail.com>
2024-03-03 12:54:51 -0400

    Se convirtio en componente la informacion de los beneficios de la empresa

---
+++ resources/views/components/eccomerce/layouts/footer/featured.blade.php
@@ -0,0 +1,81 @@
+<section class="featured section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-1-5 col-md-4 col-12 col-sm-6 mb-md-4 mb-xl-0">
+                <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp"
+                     data-wow-delay="0">
+                    <div class="banner-icon">
+                        <img src="assets/imgs/theme/icons/icon-1.svg" alt=""/>
+                    </div>
+                    <div class="banner-text">
+                        <h3 class="icon-box-title">{{__("Best prices & offers")}}</h3>
+                        {{-- <p>Orders $50 or more</p> --}}
+                        <p>{{__('Affordable prices')}}</p>
+                        <br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
+                <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp"
+                     data-wow-delay=".1s">
+                    <div class="banner-icon">
+                        <img src="assets/imgs/theme/icons/icon-2.svg" alt=""/>
+                    </div>
+                    <div class="banner-text">
+                        <h3 class="icon-box-title">{{__("Free delivery")}}</h3>
+                        <p>{{__("24/7 amazing services")}}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
+                <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp"
+                     data-wow-delay=".2s">
+                    <div class="banner-icon">
+                        <img src="assets/imgs/theme/icons/icon-3.svg" alt=""/>
+                    </div>
+                    <div class="banner-text">
+                        <h3 class="icon-box-title">{{__("Great daily deal")}}</h3>
+                        <p>{{__("When you sign up")}}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
+                <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp"
+                     data-wow-delay=".3s">
+                    <div class="banner-icon">
+                        <img src="assets/imgs/theme/icons/icon-4.svg" alt=""/>
+                    </div>
+                    <div class="banner-text">
+                        <h3 class="icon-box-title">{{__("Wide assortment")}}</h3>
+                        <p>{{__("Mega Discounts")}}</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
+                <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp"
+                     data-wow-delay=".4s">
+                    <div class="banner-icon">
+                        <img src="assets/imgs/theme/icons/icon-5.svg" alt=""/>
+                    </div>
+                    <div class="banner-text">
+                        <h3 class="icon-box-title">{{__("Easy returns")}}</h3>
+                        <p>{{__("Terms and conditions apply")}}</p>
+                    </div>
+                </div>
+            </div>
+            {{-- <div class="col-lg-1-5 col-md-4 col-12 col-sm-6 d-xl-none">
+                <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp"
+                     data-wow-delay=".5s">
+                    <div class="banner-icon">
+                        <img src="assets/imgs/theme/icons/icon-6.svg" alt=""/>
+                    </div>
+                    <div class="banner-text">
+                        <h3 class="icon-box-title">Safe delivery</h3>
+                        <p>Within 30 days</p>
+                    </div>
+                </div>
+            </div> --}}
+        </div>
+    </div>
+</section>
