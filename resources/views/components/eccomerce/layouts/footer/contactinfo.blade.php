b3de5593c5175795d4010e1e09530a5d5f50aa9b
Alberto Zarraga <zarraga555@gmail.com>
2024-03-03 12:54:23 -0400

    Se convirtio en un componente la informacion de la empresa

---
+++ resources/views/components/eccomerce/layouts/footer/contactinfo.blade.php
@@ -0,0 +1,9 @@
+<ul class="contact-infor">
+    <li><img src="assets/imgs/theme/icons/icon-location.svg" alt=""/><strong>{{__("Address:")}} </strong>
+        <span>5171 W Campbell Ave undefined Kent, Utah 53127 United States</span></li>
+    <li><img src="assets/imgs/theme/icons/icon-contact.svg" alt=""/><strong>{{__("Call Us")}} </strong><span>(+91) - 540-025-124553</span></li>
+    <li><img src="assets/imgs/theme/icons/icon-email-2.svg"
+             alt=""/><strong>{{__("Email:")}} </strong><span>sale@Nest.com</span></li>
+    <li><img src="assets/imgs/theme/icons/icon-clock.svg" alt=""/><strong>{{__("Hours:")}} </strong><span>10:00 - 18:00, Mon - Sat</span>
+    </li>
+</ul>
