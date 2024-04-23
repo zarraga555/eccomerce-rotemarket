bf6d0628cab6f487e2061a9b0da49074c01b0460
Alberto Zarraga <zarraga555@gmail.com>
2024-04-23 01:02:36 -0400

    Se agrego el menu para la pantalla del POS

---
+++ resources/views/components/eccomerce/admin/navbar/desktop/menu.blade.php
@@ -0,0 +1,5 @@
+<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+{{--<a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>--}}
+<a href="#" class="text-gray-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
+<a href="#" class="text-gray-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
+<a href="#" class="text-gray-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
