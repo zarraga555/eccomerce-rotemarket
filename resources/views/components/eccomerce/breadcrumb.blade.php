cb89b37e4158d03bcd69e8a9a9ad70952b964aa4
Alberto Zarraga <zarraga555@gmail.com>
2024-05-22 22:03:38 -0400

    Se creo este componente para reutilizar los breadcrumbs

---
+++ resources/views/components/eccomerce/breadcrumb.blade.php
@@ -0,0 +1,9 @@
+<div class="page-header breadcrumb-wrap">
+    <div class="container">
+        <div class="breadcrumb">
+            <a href="/" rel="nofollow"><i class="fi-rs-home mr-5"></i>{{__("Home")}}</a>
+            {{ $breadcrumbs }}
+
+        </div>
+    </div>
+</div>
