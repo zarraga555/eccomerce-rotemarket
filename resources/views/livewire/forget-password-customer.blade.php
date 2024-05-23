05074cddcc35713f5dd68824568dfd199b7be6b8
Alberto Zarraga <zarraga555@gmail.com>
2024-05-22 21:59:35 -0400

    Se creo la recuperacion de contraseña

---
+++ resources/views/livewire/forget-password-customer.blade.php
@@ -0,0 +1,28 @@
+<div>
+    <main class="main pages">
+        <x-eccomerce.breadcrumb>
+            <x-slot name="breadcrumbs">
+                <span></span> {{__("My Account")}}
+            </x-slot>
+        </x-eccomerce.breadcrumb>
+
+        <div class="page-content pt-50 pb-50">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-6 col-md-12 m-auto">
+                        <div class="login_wrap widget-taber-content background-white">
+                            <div class="padding_eight_all bg-white">
+                                <div class="heading_s1">
+                                    <img class="border-radius-15" src="assets/imgs/page/forgot_password.svg" alt="password"/>
+                                    <h2 class="mb-15 mt-15">{{__("Forgot your password?")}}</h2>
+                                    <p class="mb-30">{{__("Not to worry, we got you! Let’s get you a new password. Please enter your email address or your Username.")}}</p>
+                                </div>
+                                <x-eccomerce.users.forgot.form/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</div>
