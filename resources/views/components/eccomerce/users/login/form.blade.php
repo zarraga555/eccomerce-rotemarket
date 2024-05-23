8fbb9edec450cad12b804086dd62c2350d0d0f73
Alberto Zarraga <zarraga555@gmail.com>
2024-05-22 22:01:50 -0400

    Se cinvirtio en un compoenente el formilario para el inicio de sesion

---
+++ resources/views/components/eccomerce/users/login/form.blade.php
@@ -0,0 +1,32 @@
+<form method="post">
+    <div class="form-group">
+        <input type="text" required name="email" placeholder="{{__("Username or Email *")}}" />
+    </div>
+    <div class="form-group">
+        <input required type="password" name="password" placeholder="{{__("Your password *")}}"/>
+    </div>
+    {{--                                            Captcha commit--}}
+    {{--                                            <div class="login_footer form-group">--}}
+    {{--                                                <div class="chek-form">--}}
+    {{--                                                    <input type="text" required="" name="email" placeholder="Security code *" />--}}
+    {{--                                                </div>--}}
+    {{--                                                <span class="security-code">--}}
+    {{--                                                    <b class="text-new">8</b>--}}
+    {{--                                                    <b class="text-hot">6</b>--}}
+    {{--                                                    <b class="text-sale">7</b>--}}
+    {{--                                                    <b class="text-best">5</b>--}}
+    {{--                                                </span>--}}
+    {{--                                            </div>--}}
+    <div class="login_footer form-group mb-50">
+        <div class="chek-form">
+            <div class="custome-checkbox">
+                <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox1" value="" />
+                <label class="form-check-label" for="exampleCheckbox1"><span>{{__("Remember me")}}</span></label>
+            </div>
+        </div>
+        <a class="text-muted" href="{{ route('forgetPasswordCustomer') }}">{{__("Forgot password?")}}</a>
+    </div>
+    <div class="form-group">
+        <button type="submit" class="btn btn-heading btn-block hover-up" name="login">{{__("Login")}}</button>
+    </div>
+</form>
