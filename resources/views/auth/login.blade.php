9000bc4086264e7f761f19c05e4af27b4e7b5777
Alberto Zarraga <zarraga555@gmail.com>
2024-06-06 12:56:13 -0400

    Se remplazo el login por el nuevo diseño

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,4 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+<x-app-layout>
 
         <x-validation-errors class="mb-4" />
 
@@ -12,37 +8,33 @@
             </div>
         @endif
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+        <main class="main pages">
+            <x-eccomerce.breadcrumb>
+                <x-slot name="breadcrumbs">
+                    <span></span> {{__("My Account")}}
+                </x-slot>
+            </x-eccomerce.breadcrumb>
+            <div class="page-content pt-100 pb-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-8 col-lg-10 col-md-12 m-auto">
+                            <div class="row">
+                                <x-eccomerce.users.image url="assets/imgs/page/login-1.png" alt="login"/>
+                                <div class="col-lg-6 col-md-8">
+                                    <div class="login_wrap widget-taber-content background-white">
+                                        <div class="padding_eight_all bg-white">
+                                            <div class="heading_s1">
+                                                <h1 class="mb-5">{{__("Login")}}</h1>
+                                                <p class="mb-30">{{__("Don't have an account?")}} <a href="{{ route('register') }}">{{__("Create here")}}</a></p>
+                                            </div>
+                                            <x-eccomerce.users.login.form/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+        </main>
+</x-app-layout>
