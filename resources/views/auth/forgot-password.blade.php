54ba49cc9b666c5cc3801c0ac11c0d2fbc1befca
Alberto Zarraga <zarraga555@gmail.com>
2024-06-06 12:55:58 -0400

    Se remplazao fogot password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,40 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+<x-app-layout>
 
-        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    @if (session('status'))
+        <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+            {{ session('status') }}
         </div>
+    @endif
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-                {{ session('status') }}
+    <x-validation-errors class="mb-4"/>
+
+    <main class="main pages">
+        <x-eccomerce.breadcrumb>
+            <x-slot name="breadcrumbs">
+                <span></span> {{__("My Account")}}
+            </x-slot>
+        </x-eccomerce.breadcrumb>
+
+        <div class="page-content pt-50 pb-50">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-6 col-md-12 m-auto">
+                        <div class="login_wrap widget-taber-content background-white">
+                            <div class="padding_eight_all bg-white">
+                                <div class="heading_s1">
+                                    <img class="border-radius-15" src="assets/imgs/page/forgot_password.svg"
+                                         alt="password"/>
+                                    <h2 class="mb-15 mt-15">{{__("Forgot your password?")}}</h2>
+                                    <p class="mb-30">{{__("Not to worry, we got you! Let’s get you a new password. Please enter your email address or your Username.")}}</p>
+                                </div>
+                                <x-eccomerce.users.forgot.form/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        @endif
+        </div>
+    </main>
 
-        <x-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <div class="block">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+</x-app-layout>
