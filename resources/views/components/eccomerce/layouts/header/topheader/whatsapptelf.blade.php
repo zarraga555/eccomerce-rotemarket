f80f6a90f611069a4fa94d6992d4900f14cd18b4
Alberto Zarraga <zarraga555@gmail.com>
2024-06-07 15:18:21 -0400

    Se hizo una modificacion de estilo al icono de whatsapp para su funcionamiento

--- resources/views/components/eccomerce/layouts/header/topheader/whatsapptelf.blade.php
+++ resources/views/components/eccomerce/layouts/header/topheader/whatsapptelf.blade.php
@@ -1,5 +1,5 @@
 <li>
-    <img class="svgInject" src="{{asset('assets/imgs/theme/icons/whatsapp.svg')}}" alt="whatsapp image" width="26px" style="fill: #3BB77E !important">
+    <img class="svgInject" src="{{asset('assets/imgs/theme/icons/whatsapp.svg')}}" alt="whatsapp image" width="20px" style="fill: #3BB77E !important">
     <a href="https://wa.me/59176917812" target="_blank" style="padding-left: 4px">
         {{__('Need help? Call Us:')}} <strong class="text-brand"> + 1800 900</strong>
     </a>
