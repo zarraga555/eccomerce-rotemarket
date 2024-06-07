50a92afc51305ccd0faec2d169d59ff983b3e069
Alberto Zarraga <zarraga555@gmail.com>
2024-06-07 15:20:43 -0400

    Se agregagron los variables de auth y guest para este componente

--- resources/views/components/eccomerce/layouts/header/middleheader/cart/badge.blade.php
+++ resources/views/components/eccomerce/layouts/header/middleheader/cart/badge.blade.php
@@ -1,4 +1,13 @@
-<a class="mini-cart-icon" href="shop-cart.html">
-    <img alt="Nest" src="assets/imgs/theme/icons/icon-cart.svg"/>
-    <span class="pro-count blue">2</span>
-</a>
+@auth
+    <a class="mini-cart-icon" href="shop-cart.html">
+        <img alt="Nest" src="assets/imgs/theme/icons/icon-cart.svg"/>
+        <span class="pro-count blue">2</span>
+    </a>
+@endauth
+
+@guest
+    <a class="mini-cart-icon" href="shop-cart.html">
+        <img alt="Nest" src="assets/imgs/theme/icons/icon-cart.svg"/>
+        <span class="pro-count blue">0</span>
+    </a>
+@endguest
--- resources/views/components/eccomerce/layouts/header/middleheader/cart/cartdetails.blade.php
+++ resources/views/components/eccomerce/layouts/header/middleheader/cart/cartdetails.blade.php
@@ -1,26 +1,32 @@
-<li>
-    <div class="shopping-cart-img">
-        <a href="shop-product-right.html"><img alt="Nest"
-                                               src="assets/imgs/shop/thumbnail-3.jpg"/></a>
-    </div>
-    <div class="shopping-cart-title">
-        <h4><a href="shop-product-right.html">Daisy Casual Bag</a></h4>
-        <h4><span>1 × </span>$800.00</h4>
-    </div>
-    <div class="shopping-cart-delete">
-        <a href="#"><i class="fi-rs-cross-small"></i></a>
-    </div>
-</li>
-<li>
-    <div class="shopping-cart-img">
-        <a href="shop-product-right.html"><img alt="Nest"
-                                               src="assets/imgs/shop/thumbnail-2.jpg"/></a>
-    </div>
-    <div class="shopping-cart-title">
-        <h4><a href="shop-product-right.html">Corduroy Shirts</a></h4>
-        <h4><span>1 × </span>$3200.00</h4>
-    </div>
-    <div class="shopping-cart-delete">
-        <a href="#"><i class="fi-rs-cross-small"></i></a>
-    </div>
-</li>
+@auth
+    <li>
+        <div class="shopping-cart-img">
+            <a href="shop-product-right.html"><img alt="Nest"
+                                                   src="assets/imgs/shop/thumbnail-3.jpg"/></a>
+        </div>
+        <div class="shopping-cart-title">
+            <h4><a href="shop-product-right.html">Daisy Casual Bag</a></h4>
+            <h4><span>1 × </span>$800.00</h4>
+        </div>
+        <div class="shopping-cart-delete">
+            <a href="#"><i class="fi-rs-cross-small"></i></a>
+        </div>
+    </li>
+    <li>
+        <div class="shopping-cart-img">
+            <a href="shop-product-right.html"><img alt="Nest"
+                                                   src="assets/imgs/shop/thumbnail-2.jpg"/></a>
+        </div>
+        <div class="shopping-cart-title">
+            <h4><a href="shop-product-right.html">Corduroy Shirts</a></h4>
+            <h4><span>1 × </span>$3200.00</h4>
+        </div>
+        <div class="shopping-cart-delete">
+            <a href="#"><i class="fi-rs-cross-small"></i></a>
+        </div>
+    </li>
+@endauth
+
+@guest
+    <p>{{__('You have not added any products that you are waiting for, log in and start shopping')}}</p>
+@endguest
