cc04b9d183019de27f43d797a2f5eb43ac53a4b4
Alberto Zarraga <zarraga555@gmail.com>
2024-06-10 02:43:03 -0400

    Se convirtio en componnente la vista de actualizacion de datos del usuario

---
+++ resources/views/components/eccomerce/account/accountDetail.blade.php
@@ -0,0 +1,45 @@
+<div class="tab-pane fade" id="account-detail" role="tabpanel" aria-labelledby="account-detail-tab">
+    <div class="card">
+        <div class="card-header">
+            <h5>Account Details</h5>
+        </div>
+        <div class="card-body">
+            <p>Already have an account? <a href="page-login.html">Log in instead!</a></p>
+            <form method="post" name="enq">
+                <div class="row">
+                    <div class="form-group col-md-6">
+                        <label>First Name <span class="required">*</span></label>
+                        <input required="" class="form-control" name="name" type="text" />
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label>Last Name <span class="required">*</span></label>
+                        <input required="" class="form-control" name="phone" />
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label>Display Name <span class="required">*</span></label>
+                        <input required="" class="form-control" name="dname" type="text" />
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label>Email Address <span class="required">*</span></label>
+                        <input required="" class="form-control" name="email" type="email" />
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label>Current Password <span class="required">*</span></label>
+                        <input required="" class="form-control" name="password" type="password" />
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label>New Password <span class="required">*</span></label>
+                        <input required="" class="form-control" name="npassword" type="password" />
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label>Confirm Password <span class="required">*</span></label>
+                        <input required="" class="form-control" name="cpassword" type="password" />
+                    </div>
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-fill-out submit font-weight-bold" name="submit" value="Submit">Save Change</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
