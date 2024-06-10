55ddd65b290ff2ab361a7e6c6fd178772a56e1d0
Alberto Zarraga <zarraga555@gmail.com>
2024-06-10 02:42:29 -0400

    Se convirtio en componnente la vista del dashboard la bienvenida al usuario

---
+++ resources/views/components/eccomerce/account/dashboard.blade.php
@@ -0,0 +1,13 @@
+<div class="tab-pane fade active show" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="mb-0">Hello Rosie!</h3>
+        </div>
+        <div class="card-body">
+            <p>
+                From your account dashboard. you can easily check &amp; view your <a href="#">recent orders</a>,<br />
+                manage your <a href="#">shipping and billing addresses</a> and <a href="#">edit your password and account details.</a>
+            </p>
+        </div>
+    </div>
+</div>
